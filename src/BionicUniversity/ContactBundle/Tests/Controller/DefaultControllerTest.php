c03bf294476ca207b0a051fa76ef6358d7a706bb
roman-curse <frspm.roman@gmail.com>
2014-06-10 21:09:20 +0300

    any

--- src/BionicUniversity/ContactBundle/Tests/Controller/DefaultControllerTest.php
+++ src/BionicUniversity/ContactBundle/Tests/Controller/DefaultControllerTest.php
@@ -12,6 +12,6 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/contactus');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Crazy Form")')->count() > 0);
     }
 }
